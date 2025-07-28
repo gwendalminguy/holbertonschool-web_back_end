0f9667956029c5c1e440e234a88a04ab2c78ce1f
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-28 13:36:38 +0200

    Created type-annotated function computing floor value of a float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+2-floor.py
+Module containing function to compute the floor of a float.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Computes the floor of a float.
+    """
+    return math.floor(n)
