63058c951adfcaab096e4ddedfc24035e8d87f08
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-28 19:31:44 +0200

    Created function returning a function multiplying a number by a multiplier

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+8-make_multiplier.py
+Module containing function returning a tuple.
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Returns a function multiplying a float by a given multiplier.
+    """
+    def function(n: float) -> float:
+        return n * multiplier
+
+    return function
