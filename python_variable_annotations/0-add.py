35035b12963d82820a99e14a8cb19373d2a75c11
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-28 13:25:28 +0200

    Created type-annotated function add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+0-add.py
+Module containing function to compute a sum.
+"""
+
+
+def add(a: int, b: int) -> int:
+    """
+    Computes the sum of two integers.
+    """
+    return a + b
