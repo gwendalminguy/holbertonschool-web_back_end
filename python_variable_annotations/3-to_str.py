ed697b20d002025e80c8f02fdd8f01550ee725af
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-28 18:00:55 +0200

    Created function getting a string from a float

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+3-to_str.py
+Module containing function to get string from float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Transforms a float into a string.
+    """
+    return str(n)
