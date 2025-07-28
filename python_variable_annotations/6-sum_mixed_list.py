a665d23d0a40923a6d8a07e65bc0a98f4da2bd57
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-28 18:56:31 +0200

    Creannotated function computing the sum of a float and int list

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+6-sum_mixed_list.py
+Module containing function to compute the sum of a float and int list.
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float | int]]) -> float:
+    """
+    Computes the sum of a float and int list.
+    """
+    sum: float = 0
+    for num in mxd_lst:
+        sum += num
+    return sum
