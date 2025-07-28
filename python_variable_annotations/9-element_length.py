b25bb7a3e6f42c0663c5384f0ba6dd6e913e03a2
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-28 20:36:34 +0200

    Created annotated function returning a tuple list

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+9-element_length.py
+Module containing function returning a tuple list.
+"""
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]
