4452cd973f3c42e3bfb3defb07303deecc97085f
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-29 18:00:25 +0200

    Corrected documentation

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 1-concurrent_coroutines.py
-Module containing an asynchronous rcoroutine.
+Module containing an asynchronous coroutine.
 """
 import asyncio
 import random
