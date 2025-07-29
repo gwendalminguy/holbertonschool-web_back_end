1d0c78467af5ac6862321cd5abfd1e373daf1d73
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-29 18:08:59 +0200

    Corrected variable annotation

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -10,10 +10,10 @@ import time
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> asyncio.task:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     Returns a task.
     """
-    task: asyncio.task = asyncio.create_task(wait_random(max_delay))
+    task: asyncio.Task = asyncio.create_task(wait_random(max_delay))
 
     return task
