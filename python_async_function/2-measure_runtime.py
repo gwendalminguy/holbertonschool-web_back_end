b74b549fdd0a3f0e288cedf4a8eb5d34be3bf91d
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-29 18:00:10 +0200

    Created function measuring an average runtime length

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+2-measure_runtime.py
+Module containing an asynchronous coroutine.
+"""
+import asyncio
+import random
+from typing import List
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int = 10) -> List[float]:
+    """
+    Executes wait_random n times.
+    """
+    start: float = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end: float = time.time()
+
+    length: float = end - start
+    
+    return length / n
