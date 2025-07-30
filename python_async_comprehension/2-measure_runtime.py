feda6f36e714b417425a21ba09ae2dfa93b9e6e2
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-30 10:48:41 +0200

    Created function computing runtime for the execution of four concurrent identical coroutines

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+2-measure_runtime.py
+Module containing an asynchronous coroutine.
+"""
+from typing import Generator, List
+import random
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Computes runtime for four concurrent coroutines.
+    """
+    start: float = time.time()
+
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+    )
+
+    end: float = time.time()
+
+    return end - start
