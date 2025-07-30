6ffedee529991bc9a49bb152f2fe1bf767de8fb1
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-30 10:08:14 +0200

    Created function returning a list of ten random numbers

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+1-async_comprehension.py
+Module containing an asynchronous coroutine.
+"""
+from typing import Generator, List
+import random
+import asyncio
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> Generator[float, None, None]:
+    """
+    Waits for 10 seconds and yields 10 numbers.
+    """
+    result: List = [x  async for x in async_generator()]
+    return result
