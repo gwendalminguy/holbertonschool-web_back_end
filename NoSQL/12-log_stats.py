4a80cbe46aeadd8e3bc2c86063a49fdbde306d38
gwendalminguy <gwendalminguy@outlook.fr>
2025-08-13 14:14:22 +0200

    Created script providing statistics about Nginx logs stored in MongoDB

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+12-log_stats.py
+Script providing statistics about Nginx logs stored in MongoDB.
+"""
+from pymongo import MongoClient
+
+
+def main():
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+
+    cursor = nginx_collection.find({'method': 'GET'})
+    get_count = len(list(cursor))
+
+    cursor = nginx_collection.find({'method': 'POST'})
+    post_count = len(list(cursor))
+
+    cursor = nginx_collection.find({'method': 'PUT'})
+    put_count = len(list(cursor))
+
+    cursor = nginx_collection.find({'method': 'PATCH'})
+    patch_count = len(list(cursor))
+
+    cursor = nginx_collection.find({'method': 'DELETE'})
+    delete_count = len(list(cursor))
+
+    cursor = nginx_collection.find({'path': '/status'})
+    status = len(list(cursor))
+
+    total = get_count + post_count + put_count + patch_count + delete_count
+
+    print('{} logs'.format(total))
+    print('Method:')
+    print('\tmethod GET: {}'.format(get_count))
+    print('\tmethod POST: {}'.format(post_count))
+    print('\tmethod PUT: {}'.format(put_count))
+    print('\tmethod PATCH: {}'.format(patch_count))
+    print('\tmethod DELETE: {}'.format(delete_count))
+    print('{} status check'.format(status))
+
+
+if __name__ == "__main__":
+    main()
