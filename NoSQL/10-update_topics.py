f83086f143e0f600254f441075f271e2c4c86096
gwendalminguy <gwendalminguy@outlook.fr>
2025-08-13 11:28:36 +0200

    Created function changing all topics of a school based on the name

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+10-update_topics.py
+Module containing the function update_topics.
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school based on the name.
+    """
+    document = { 'name': name }
+    values = { '$set': { 'topics': [topic for topic in topics] } }
+    mongo_collection.update_one(document, values)
