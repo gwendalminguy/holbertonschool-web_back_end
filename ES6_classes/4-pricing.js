f32a93a3baad814888b1c36677604b6aefe74c9f
gwendalminguy <gwendalminguy@outlook.fr>
2025-08-19 13:12:22 +0200

    Created class Pricing

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,48 @@
+/* eslint no-underscore-dangle: 0 */
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw TypeError('Currency must be a Currency instance');
+    }
+
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Amount
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  // Currency
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw TypeError('Currency must be a Currency instance');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
