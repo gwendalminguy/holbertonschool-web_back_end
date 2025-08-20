1a20ca5a69af94c24f944d4e84af4b0fce70fcf5
gwendalminguy <gwendalminguy@outlook.fr>
2025-08-20 08:49:34 +0200

    Created class Building

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,26 @@
+/* eslint no-underscore-dangle: 0 */
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw TypeError('SQFT must be a number');
+    }
+
+    this._sqft = sqft;
+  }
+
+  // SQFT
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw TypeError('SQFT must be a number');
+    }
+    this._sqft = value;
+  }
+
+  evacuationWarningMessage() {
+	throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
