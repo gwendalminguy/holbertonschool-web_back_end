705d86febff40619eece373cad919b0bb7a9f5b1
gwendalminguy <gwendalminguy@outlook.fr>
2025-08-20 10:23:24 +0200

    Created class Car

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,22 @@
+/* eslint no-underscore-dangle: 0 */
+export default class Car {
+  constructor(brand, motor, color) {
+    if (typeof brand !== 'string') {
+      throw TypeError('Brand must be a string');
+    }
+    if (typeof motor !== 'string') {
+      throw TypeError('Motor must be a string');
+    }
+    if (typeof color !== 'string') {
+      throw TypeError('Color must be a string');
+    }
+
+    this._brand = brand;
+    this._motor = motor;
+	this._color = color;
+  }
+
+  cloneCar() {
+	return new this.constructor(this._brand, this._motor, this._color);
+  }
+}
