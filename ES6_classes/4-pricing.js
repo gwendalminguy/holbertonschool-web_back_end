2597a8ad7828166f87ba66cbf5e2b2b6f946f116
gwendalminguy <gwendalminguy@outlook.fr>
2025-08-21 09:51:24 +0200

    Removed useless conditions in constructor

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -3,15 +3,8 @@ import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    if (typeof amount !== 'number') {
-      throw TypeError('Amount must be a number');
-    }
-    if (!(currency instanceof Currency)) {
-      throw TypeError('Currency must be a Currency instance');
-    }
-
-    this._amount = amount;
-    this._currency = currency;
+    this.amount = amount;
+    this.currency = currency;
   }
 
   // Amount
