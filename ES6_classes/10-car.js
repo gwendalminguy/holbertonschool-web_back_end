47194381fe79d9844cca4a1b640f654cf731222c
gwendalminguy <gwendalminguy@outlook.fr>
2025-08-21 10:05:41 +0200

    Removed type checking in constructor

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,22 +1,12 @@
 /* eslint no-underscore-dangle: 0 */
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string') {
-      throw TypeError('Brand must be a string');
-    }
-    if (typeof motor !== 'string') {
-      throw TypeError('Motor must be a string');
-    }
-    if (typeof color !== 'string') {
-      throw TypeError('Color must be a string');
-    }
-
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
   cloneCar() {
-    return new this.constructor(this._brand, this._motor, this._color);
+    return new this.constructor();
   }
 }
