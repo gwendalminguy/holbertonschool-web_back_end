ade2c40c32b97da92c0168b68af461eca7c62879
gwendalminguy <gwendalminguy@outlook.fr>
2025-08-25 13:12:17 +0200

    Created function concatenating strings based on substring presence in a set

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+export default function cleanSet(set, startString) {
+  let result = '';
+  const size = startString.length;
+
+  if (startString) {
+    for (const element of set) {
+      if (element.startsWith(startString)) {
+		if (result) {
+          result = `${result}-${element.substring(size)}`;
+		} else {
+          result = element.substring(size);
+		}
+      }
+    }
+  }
+  return result;
+}
