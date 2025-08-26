4aa66142d1a4ffe85d0262a88b176d3c48e4184a
gwendalminguy <gwendalminguy@outlook.fr>
2025-08-26 12:42:56 +0200

    Removed useless variables

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const promiseA = uploadPhoto();
-  const promiseB = createUser();
-
   return Promise.all(
-    [promiseA, promiseB],
+    [uploadPhoto(), createUser()],
   ).then(
     ([valueA, valueB]) => { console.log(valueA.body, valueB.firstName, valueB.lastName); },
   ).catch(
