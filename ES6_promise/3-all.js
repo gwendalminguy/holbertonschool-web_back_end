913ac39de9cd3eb80236d19dd78661c70cbea421
gwendalminguy <gwendalminguy@outlook.fr>
2025-08-26 12:58:05 +0200

    Used formatted string

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,6 +2,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then(([valueA, valueB]) => { console.log(valueA.body, valueB.firstName, valueB.lastName); })
+    .then(([valueA, valueB]) => { console.log(`${valueA.body} ${valueB.firstName} ${valueB.lastName}`); })
     .catch(() => { console.log('Signup system offline'); });
 }
