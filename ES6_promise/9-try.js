68f5c96ac348f95eed8d43e56f51cab9e9edab8e
gwendalminguy <gwendalminguy@outlook.fr>
2025-08-26 15:29:01 +0200

    Created function executing math operation

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  let result;
+  try {
+    result = mathFunction();
+  } catch (error) {
+    result = `${error.name}: ${error.message}`;
+  }
+  queue.push(result);
+  queue.push('Guardrail was processed');
+  return queue;
+}
