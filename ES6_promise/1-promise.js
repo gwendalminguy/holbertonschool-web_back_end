3449ac20e4138e44ecbf11f06702c36b8e65ad45
gwendalminguy <gwendalminguy@outlook.fr>
2025-08-26 10:18:05 +0200

    Created function returning resolved or rejected promise

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+export default function getFullResponseFromAPI(success) {
+  if (success) {
+    return Promise.resolve({
+      status: 200,
+      body: 'Success',
+    });
+  } else {
+    return Promise.reject(new Error('The fake API is not working currently'));
+  }
+}
