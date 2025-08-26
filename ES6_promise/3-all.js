08d56647361af8fcb406232e99b840a201b52356
gwendalminguy <gwendalminguy@outlook.fr>
2025-08-26 11:52:08 +0200

    Created function resolving several promises

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,18 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const promiseA = uploadPhoto();
+  const promiseB = createUser();
+
+  return Promise.all(
+    [promiseA, promiseB],
+  ).then(
+    ([valueA, valueB]) => {
+      console.log(valueA.body, valueB.firstName, valueB.lastName);
+    },
+  ).catch(
+    () => {
+      console.log('Signup system offline');
+    },
+  );
+}
