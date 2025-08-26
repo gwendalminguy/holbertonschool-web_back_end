bd49e829c1c16b6404721e820379ef19c296a7bb
gwendalminguy <gwendalminguy@outlook.fr>
2025-08-26 12:46:33 +0200

    Corrected with eslint

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all(
-    [uploadPhoto(), createUser()],
-  ).then(
-    ([valueA, valueB]) => { console.log(valueA.body, valueB.firstName, valueB.lastName); },
-  ).catch(
-    () => { console.log('Signup system offline'); },
-  );
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([valueA, valueB]) => { console.log(valueA.body, valueB.firstName, valueB.lastName); })
+    .catch(() => { console.log('Signup system offline'); });
 }
