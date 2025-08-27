60eaf216a84c0ea7dbf962444cfb09c0bbbec489
gwendalminguy <gwendalminguy@outlook.fr>
2025-08-27 10:24:47 +0200

    Created helper function to handle results

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,10 +1,21 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
+function handleResults(results) {
+  const array = [];
+  for (const element of results) {
+    array.push({
+      status: element.status,
+      value: element.value,
+    });
+  }
+  return array;
+}
+
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promiseUser = signUpUser(firstName, lastName);
   const promisePhoto = uploadPhoto(fileName);
 
   return Promise.allSettled([promiseUser, promisePhoto])
-    .then((results) => { console.log(results) });
+    .then((results) => { handleResults(results); });
 }
