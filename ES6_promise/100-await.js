870684ba7a380c4a951e3f60fda9a11d87fe5ec1
gwendalminguy <gwendalminguy@outlook.fr>
2025-08-27 11:32:44 +0200

    Corrected case where one promise fails

--- ES6_promise/100-await.js
+++ ES6_promise/100-await.js
@@ -2,5 +2,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then(([photo, user]) => { return {photo, user} });
+    .then(([photo, user]) => { return {photo, user} })
+    .catch(() => { return {photo: null, user: null}});
 }
