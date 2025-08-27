c84090173c57381adff85200b9c0cb45b33cc6b5
gwendalminguy <gwendalminguy@outlook.fr>
2025-08-27 09:10:27 +0200

    Tried with an array

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,14 @@
 export default function cleanSet(set, startString) {
-  let result = '';
+  let result = [];
   const size = startString.length;
 
   if (startString) {
     for (const element of set) {
       if (element.startsWith(startString)) {
-        if (result) {
-          result = `${result}-${element.substring(size)}`;
+        if (result.length) {
+          result.push(`-${element.substring(size)}`);
         } else {
-          result = element.substring(size);
+          result.push(element.substring(size));
         }
       }
     }
