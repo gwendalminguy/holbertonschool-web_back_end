58c2defd0281b72e170d61606025697faa0d4459
gwendalminguy <gwendalminguy@outlook.fr>
2025-09-01 15:54:15 +0200

    Created http simple server

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer(async (req, res) => {
+  const file = process.argv[2];
+  const { url } = req;
+
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  if (url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (url === '/students') {
+    await countStudents(file)
+      .then((data) => res.end(`This is the list of our students\n${data}`))
+      .catch((err) => res.end(`Error: ${err.message}`));
+  }
+});
+
+app.listen(1245, 'localhost', () => {
+  console.log('Server running at http://localhost:1245/');
+});
+
+module.exports = app;
