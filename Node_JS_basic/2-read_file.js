affc9694f3bf92f07c7ea9c1966b875fd973db7f
gwendalminguy <gwendalminguy@outlook.fr>
2025-09-01 13:34:04 +0200

    Switch readFile to readFileSync

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,33 +1,34 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  fs.readFile(path, 'utf8', (err, data) => {
-    if (err) {
-      throw new Error('Cannot load the database');
+  let data = '';
+  try {
+    data = fs.readFileSync(path, 'utf8');
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+
+  const content = data.split('\n');
+  const fields = {};
+  let total = 0;
+
+  for (let i = 1; i < content.length; i += 1) {
+    const [firstname, , , field] = content[i].split(',');
+
+    if (!(field in fields) && field) {
+      fields[field] = [];
     }
 
-    const content = data.split('\n');
-    const fields = {};
-    let total = 0;
-
-    for (let i = 1; i < content.length; i += 1) {
-      const [firstname, , , field] = content[i].split(',');
-
-      if (!(field in fields) && field) {
-        fields[field] = [];
-      }
-
-      if (field) {
-        fields[field].push(firstname);
-        total += 1;
-      }
+    if (field) {
+      fields[field].push(firstname);
+      total += 1;
     }
+  }
 
-    console.log(`Number of students: ${total}`);
-    for (const [key] of Object.entries(fields)) {
-      console.log(`Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}`);
-    }
-  });
+  console.log(`Number of students: ${total}`);
+  for (const [key] of Object.entries(fields)) {
+    console.log(`Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}`);
+  }
 }
 
 module.exports = countStudents;
