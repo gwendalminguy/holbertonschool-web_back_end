a0c566e2776de7d6f2002b94e7eccd2e553a547f
gwendalminguy <gwendalminguy@outlook.fr>
2025-09-01 10:26:15 +0200

    Corrected program to print message when not interrupted manually

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -7,9 +7,11 @@ const rl = readline.createInterface({
 
 rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
+  rl.close();
 });
 
-rl.on('SIGINT', () => {
-  console.log('This important software is now closing');
-  rl.close();
+rl.on('close', () => {
+  if (!process.stdin.setRawMode) {
+    console.log('This important software is now closing');
+  }
 });
