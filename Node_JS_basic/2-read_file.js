8dc5b17a48837615175f55ce3722d96fc030dc79
gwendalminguy <gwendalminguy@outlook.fr>
2025-09-01 12:06:43 +0200

    Corrected with eslint

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,30 +1,30 @@
-function countStudents(path) {
-  const fs = require('fs');
+const fs = require('fs');
 
+function countStudents(path) {
   fs.readFile(path, 'utf8', (err, data) => {
-	if (err) {
-		throw new Error('Cannot load the database');
-	}
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
 
     const content = data.split('\n');
-	const CSStudents = [];
-	const SWEStudents = [];
+    const CSStudents = [];
+    const SWEStudents = [];
 
-	for (let i = 1; i < content.length; i++) {
+    for (let i = 1; i < content.length; i += 1) {
       const [firstname, lastname, age, field] = content[i].split(',');
-	  if (field === 'CS') {
-		CSStudents.push(firstname);
-	  } else if (field === 'SWE') {
-		SWEStudents.push(firstname);
-	  }
-	}
+      if (field === 'CS') {
+        CSStudents.push(firstname);
+      } else if (field === 'SWE') {
+        SWEStudents.push(firstname);
+      }
+    }
 
-	const stringCS = CSStudents.join(', ');
-	const stringSWE = SWEStudents.join(', ');
+    const stringCS = CSStudents.join(', ');
+    const stringSWE = SWEStudents.join(', ');
 
-	console.log(`Number of students: ${CSStudents.length + SWEStudents.length}`);
-	console.log(`Number of students in CS: ${CSStudents.length}. List: ${stringCS}`);
-	console.log(`Number of students in SWE: ${SWEStudents.length}. List: ${stringSWE}`);
+    console.log(`Number of students: ${CSStudents.length + SWEStudents.length}`);
+    console.log(`Number of students in CS: ${CSStudents.length}. List: ${stringCS}`);
+    console.log(`Number of students in SWE: ${SWEStudents.length}. List: ${stringSWE}`);
   });
 }
 
