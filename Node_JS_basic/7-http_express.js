a51e9a7e67e5ec2b1134d13f993ebc2df499a783
gwendalminguy <gwendalminguy@outlook.fr>
2025-09-02 09:24:25 +0200

    Created http simple server using Express module

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  const file = process.argv[2];
+  const { url } = req;
+
+  res.set('Content-Type', 'text/plain');
+  await countStudents(file)
+    .then((data) => res.send(`This is the list of our students\n${data}`))
+    .catch((err) => res.send(err.toString()));
+});
+
+app.listen(port, () => {
+  console.log('Server running at http://localhost:1245/');
+});
+
+module.exports = app;
