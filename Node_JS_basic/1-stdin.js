b295ee560c3c2d23c306fca8890bf15c802b2ffb
gwendalminguy <gwendalminguy@outlook.fr>
2025-09-04 09:46:59 +0200

    Simplified code

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,4 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n', () => {
-  process.stdin.on('data', (name) => {
-    process.stdout.write(`Your name is: ${name}`);
-    if (!process.stdin.setRawMode) {
-      process.stdout.write('This important software is now closing\n');
-    }
-  });
+  process.stdin.on('data', (name) => process.stdout.write(`Your name is: ${name}`));
+  process.stdin.on('end', () => process.stdout.write('This important software is now closing\n'));
 });
