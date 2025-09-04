3b37bb80dea7e3cc283a50381c76a04735adb001
gwendalminguy <gwendalminguy@outlook.fr>
2025-09-04 09:27:26 +0200

    Replaced console.log with process.stdout.write and readline.question with process.stdin.on

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,16 +1,8 @@
-const readline = require('node:readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-});
-
-rl.on('close', () => {
-  if (!process.stdin.setRawMode) {
-    console.log('This important software is now closing');
-  }
+process.stdout.write('Welcome to Holberton School, what is your name?\n', () => {
+  process.stdin.on('data', (name) => {
+    process.stdout.write(`Your name is: ${name}`);
+    if (!process.stdin.setRawMode) {
+      process.stdout.write('This important software is now closing\n');
+    }
+  });
 });
