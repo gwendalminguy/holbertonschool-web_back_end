056a82b122714ba7c2c55563aa89db20664223c6
Gwendal <gwendalminguy@outlook.fr>
2026-07-29 21:09:02 +0200

    Optimize put method by checking only once if key is present in cache_data

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -17,13 +17,12 @@ class LIFOCache(BaseCaching):
         if key is not None and item is not None:
             current_size = len(self.cache_data)
 
-            # Discard only if item is new and maximum cache size is reachded
-            if key not in self.cache_data and current_size >= self.MAX_ITEMS:
+            # Discard item only if it is new and maximum cache size is reachded
+            if key not in self.cache_data and len(self.cache_data) >= self.MAX_ITEMS:
                 self.discard()
 
             # Recreate element on update to keep cache order right
-            if key in self.cache_data:
-                self.cache_data.pop(key)
+            self.cache_data.pop(key, None)
 
             self.cache_data[key] = item
 
