b775d875240a62e73d633bee41188cc9b4f04162
Gwendal <gwendalminguy@outlook.fr>
2026-07-29 12:07:54 +0200

    Create FIFO cache system

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+1-fifo_cache.py
+FIFO Cache module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    BasicCache defines a simple cache.
+    """
+    def put(self, key, item):
+        """
+        Add or update an element to the cache.
+        """
+        if key and item:
+            current_size = len(self.cache_data)
+
+            # Discard only if item is new and maximum cache size is reachded
+            if key not in self.cache_data and current_size >= self.MAX_ITEMS:
+                self.discard()
+
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieve an element from the cache.
+        """
+        item = self.cache_data.get(key, None)
+
+        return item
+
+    def discard(self):
+        """
+        Discard oldest element created in cache.
+        """
+        item = next(iter(self.cache_data), None)
+
+        if item:
+            self.cache_data.pop(item)
+            print(f"DISCARD: {item}")
