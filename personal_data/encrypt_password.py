e4fb8b1803f9f52ebb29470b629474ea84aef460
Gwendal <gwendalminguy@outlook.fr>
2026-09-05 00:17:02 +0200

    Create hash_password function

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+encrypt_password.py
+Module containing the function filter_datum.
+"""
+from typing import ByteString
+
+import bcrypt
+
+
+def hash_password(password: str) -> ByteString:
+    """
+    Hash and salt a password.
+
+    Parameters:
+    - password: string to hash
+
+    Return: the hashed password
+    """
+    encoded_password = password.encode(encoding="utf-8")
+
+    salt = bcrypt.gensalt()
+
+    return bcrypt.hashpw(encoded_password, salt)
