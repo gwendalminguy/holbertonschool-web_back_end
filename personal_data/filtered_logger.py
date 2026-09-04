41f373994f0d96854641057ba57106ca821e2b1f
Gwendal <gwendalminguy@outlook.fr>
2026-09-04 15:19:28 +0200

    Implement RedactingFormatter class with a constructor and the format method

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,6 +5,7 @@ Module containing the function filter_datum.
 """
 from typing import List
 
+import logging
 import re
 
 
@@ -31,3 +32,25 @@ def filter_datum(
         message = re.sub(re.escape(item) + p, f"{item}={redaction}", message)
 
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class.
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format and filter a given record to return it redacted.
+        """
+        formatted = super().format(record)
+
+        return filter_datum(self.fields, self.REDACTION, formatted, self.SEPARATOR)
