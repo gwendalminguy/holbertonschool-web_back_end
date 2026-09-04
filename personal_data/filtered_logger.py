9e1bd12ea74141cc5881b82140e9139dd1653790
Gwendal <gwendalminguy@outlook.fr>
2026-09-04 09:54:23 +0200

    Create function obfuscating data with given redaction

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+filtered_logger.py
+Module containing the function filter_datum.
+"""
+import re
+
+
+def filter_datum(
+    fields: list[str],
+    redaction: str,
+    message: str,
+    separator: str
+) -> str:
+    """
+    Obfuscate every value in 'message' matching 'fields' keys with 'redaction'.
+    """
+    r = r"=.*?(?=" + re.escape(separator) + r")"
+
+    for item in fields:
+        message = re.sub(re.escape(item) + r, f"{item}={redaction};", message)
+
+    return message
