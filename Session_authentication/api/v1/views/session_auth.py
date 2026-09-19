b4fbf1c9f83cbe0c7049b70a30c2ecb0975f75ed
Gwendal <gwendalminguy@outlook.fr>
2026-09-19 22:45:58 +0200

    Create /auth_session/login endpoint

---
+++ Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+SessionAuth Views Module
+"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request, make_response
+from models.user import User
+
+import os
+
+SESSION_NAME = os.getenv("SESSION_NAME")
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    from api.v1.app import auth
+
+    email = request.form.get("email")
+    pwd = request.form.get("password")
+
+    if email is None or len(email) == 0:
+        abort(make_response({"error": "email missing"}, 400))
+
+    if pwd is None or len(pwd) == 0:
+        abort(make_response({"error": "password missing"}, 400))
+
+    results = User().search({"email": email})
+
+    if len(results) != 1:
+        abort(make_response({"error": "no user found for this email"}, 404))
+
+    user = results[0]
+
+    if not user.is_valid_password(pwd):
+        abort(make_response({"error": "wrong password"}, 401))
+
+    session_id = auth.create_session(user_id=user.id)
+
+    response = jsonify(user.to_json())
+    response.set_cookie(SESSION_NAME, session_id)
+
+    return response
