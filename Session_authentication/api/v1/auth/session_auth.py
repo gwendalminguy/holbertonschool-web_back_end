b772d4d042d268daba12e7e5795c6ce82a8fff8b
Gwendal <gwendalminguy@outlook.fr>
2026-09-19 23:57:25 +0200

    Add missing argument to retrieve user ID in destroy_session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -78,7 +78,7 @@ class SessionAuth(Auth):
         if session_id is None:
             return False
 
-        user_id = self.user_id_for_session_id
+        user_id = self.user_id_for_session_id(session_id)
 
         if user_id is None:
             return False
