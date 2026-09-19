7f15997af475693090a8a2c3ebacbd6f468ac976
Gwendal <gwendalminguy@outlook.fr>
2026-09-19 18:47:49 +0200

    Improve before_request to exclude login endpoint from the ones requiring to be authenticated

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -60,7 +60,8 @@ def before_request():
     excluded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/',
     ]
 
     if auth is None:
@@ -72,7 +73,8 @@ def before_request():
     ):
         return
 
-    if auth.authorization_header(request) is None:
+    if (auth.authorization_header(request) is None
+            and auth.session_cookie(request) is None):
         abort(401)
 
     request.current_user = auth.current_user(request)
